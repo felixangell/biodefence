0a7bcc3333c715e7778ac4bf6b925f40b09fe323
James McGovern <j.bmcgovern@hotmail.com>
2019-03-26 16:28:17 +0000

    Aging implemented

--- src/HUD.js
+++ src/HUD.js
@@ -9,17 +9,15 @@ export class HUD {
         const SECOND = 1000;
 
         // n seconds in a year.
-        const ageInterval
+        const ageInterval = 45 * SECOND;
         
-        if (age <= 5){ // Ages 1-5 take 45 seconds each for a total of 225 seconds or 3.75 minutes.
-            ageInterval = 45 * SECOND;
-        } else if (age > 5 && age <= 10){ // Ages 5-10 take 30 seconds each for a total of 150 seconds or 2.50 minutes.
+        if (this.age > 5 && this.age <= 10){ // Ages 5-10 take 30 seconds each for a total of 150 seconds or 2.50 minutes.
             ageInterval = 30 * SECOND;
-        } else if (age > 10 && age <= 20){ // Ages 10-20 take 20 seconds each for a total of 200 seconds or 3.33 minutes.
+        } else if (this.age > 10 && this.age <= 20){ // Ages 10-20 take 20 seconds each for a total of 200 seconds or 3.33 minutes.
             ageInterval = 20 * SECOND;       
-        } else if (age > 20 && age <= 40){// Ages 20-40 take 15 seconds each for a total of 300 seconds or 5 minutes.
+        } else if (this.age > 20 && this.age <= 40){// Ages 20-40 take 15 seconds each for a total of 300 seconds or 5 minutes.
             ageInterval = 15 * SECOND;
-        } else if (age > 40 && age <= 80){// Ages 40-80 take 10 seconds each for a total of 400 seconds or 6.66 minutes.
+        } else if (this.age > 40 && this.age <= 80){// Ages 40-80 take 10 seconds each for a total of 400 seconds or 6.66 minutes.
             ageInterval = 10 * SECOND;
         }
         
